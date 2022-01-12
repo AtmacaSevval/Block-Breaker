78504dbb69f5d4df2c5d1f8b28441cb46a1f48a7
AtmacaSevval <sevvallatmaca@gmail.com>
2022-01-12 21:27:17 +0300

    edited

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    
+    [SerializeField] int timeHits;
+    [SerializeField] Sprite[] hitSprites;
+
+    Level level;
+
+    public void Start()
+    {
+        CountBreakableBlocks();
+        
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timeHits++;
+        int maxHits = hitSprites.Length + 1;
+        if (timeHits >= maxHits)
+        {
+
+            DestroyBlock();
+        }
+        else
+        {
+            showNextHitSprites();
+        }
+    }
+
+    private void showNextHitSprites()
+    {
+        int spriteIndex = timeHits - 1;
+        if(hitSprites[spriteIndex] != null)
+        {
+            Debug.Log("asd");
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array" +gameObject.name);
+        }
+        
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroySFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
